b96148c0a7b073c5b64b0b050f377b83c7566a5f
Nishant Singh Tanwar <nishantsinghtanwar@Nishants-MacBook-Air.local>
2026-03-31 07:05:52 +0530

    completed project

--- src/App.jsx
+++ src/App.jsx
@@ -1,120 +1,123 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { use, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [isStart, setIsStart] = useState(false);
+  const [isPaused, setIsPaused] = useState(false);
+  const [hours, setHours] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [timerId, setTimerId] = useState(0);
+
+  const handleStart = () => {
+    setIsStart(true);
+    
+  }
+  const handleReset = () => {
+    setIsStart(false);
+    resetTimer()
+  }
+  const handlePause = () => {
+    setIsPaused(true);
+    clearInterval(timerId);
+  }
+  const handleResume = () => {
+    setIsPaused(true);
+    runTimer(seconds, minutes, hours);
+  }
+  const resetTimer = () => {
+    setHours(0);
+    setMinutes(0)
+    setSeconds(0);
+    clearInterval(timerId);
+  }
+  const handleInput = (e) => {
+    console.log(e.target.id , e.target.value);    
+    const id = e.target.id;
+    const value = parseInt(e.target.value);
+    if(id === 'hours'){
+      setHours(value);
+    }
+    else if (id === 'minutes'){
+      setMinutes(value);
+    }
+    else {
+      setSeconds(value);
+    }
+  }
+  // console.log(hours, minutes, seconds);
+
+  const runTimer = (sec, min, hr, tid)=> {
+    if(sec > 0){
+      setSeconds((s)=> s-1);
+    }
+    else if (sec === 0 && min > 0){
+      setMinutes((min)=> min - 1);
+      setSeconds(59);
+    }
+    else {
+      setHours((hr) => hr - 1);
+      setMinutes(59);
+      setSeconds(59);
+    }
+
+    if( sec === 0 && min === 0 && hr === 0 ){
+      resetTimer();
+    }
+  }
+
+  useEffect(()=>{
+    let tid;
+    if(isStart){
+    tid = setInterval(()=>{
+      runTimer(seconds, minutes, hours, tid);
+    }, 1000)
+
+    setTimerId(tid);
+
+    return () => {
+      clearInterval(tid);
+    }
+   }
+
+  }, [isStart, seconds, minutes, hours]);
+  
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+   <div className="App">
+    <h1>Counter Timer</h1>
+    {
+      !isStart && <div className="input-container">
+      <div className="input-box">
+        <input type="text" id="hours" placeholder='HH' onChange={(e)=> handleInput(e)} />
+        <input type="text" id="minutes" placeholder='MM' onChange={(e)=> handleInput(e)} />
+        <input type="text" id="seconds" placeholder='SS' onChange={(e)=> handleInput(e)} />
+      </div>
+      <button onClick={()=> handleStart()} className="timer-button">Start</button>
+    </div>
+    }
+    
+    {
+      isStart && <div className="show-container">
+      <div className="timer-box">
+        <div>{hours < 10 ? `0${hours}` : hours}</div>
+        <span>:</span>
+        <div>{minutes < 10 ? `0${minutes}` : minutes }</div>
+        <span>:</span>
+        <div>{seconds < 10 ? `0${seconds}` : seconds}</div>
+      </div>
+    
+    <div className="action-box">
+      { !isPaused && <button className="timer-button" onClick={()=> handlePause()}>Pause</button> }
+      { isPaused && <button className="timer-button" onClick={()=> handleResume()}>Resume</button> }
+      <button className="timer-button" onClick={()=> handleReset()} >Reset</button>
+    </div>
+    </div>
+    }
+    
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+   </div>
   )
 }
 
